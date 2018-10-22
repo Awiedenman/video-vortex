ff8df187cebaadb839b3244a16a872a64a35451d
Austin.Wiedenman <awiedenman@gmail.com>
2018-10-22 17:47:17 -0600

    Show cleaned date on page

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const cleanDate = () => {
   const date = new Date();
-  var options = { hour: "numeric", minute: "2-digit", weekday: "short", year: "numeric", month: "short", day: "numeric"};
+  const options = { hour: "numeric", minute: "2-digit", weekday: "short", year: "numeric", month: "short", day: "numeric"};
   const lastUpdated = date.toLocaleString('en-US', options);
   const splitDate = lastUpdated.split(',');
-  return splitDate[3] + ' ' + splitDate[1] + ' ' + splitDate[2]
+  const cleanDate =  splitDate[3] + ' ' + splitDate[1] + ' ' + splitDate[2]
+  const dateShownOnPage = document.querySelector(".last-updated");
+  dateShownOnPage.innerText = 'LAST UPDATED: ' + cleanDate;
 }
 
+cleanDate();
 console.log(cleanDate());
